60258e81f1832fa04dcca7fc0bc6f7325982d354
Parikh <ksp5368@psu.edu>
2022-11-07 22:52:10 -0500

    done

--- Lab2/Lab2.runs/synth_1/DataPath.tcl
+++ Lab2/Lab2.runs/synth_1/DataPath.tcl
@@ -4,7 +4,7 @@
 
 set TIME_start [clock seconds] 
 namespace eval ::optrace {
-  variable script "C:/Users/ksp5368/Documents/CMPEN331/Lab2/Lab2.runs/synth_1/testbench.tcl"
+  variable script "C:/Users/ksp5368/Documents/CMPEN331/Lab2/Lab2.runs/synth_1/DataPath.tcl"
   variable category "vivado_synth"
 }
 
@@ -85,9 +85,8 @@ set_property ip_cache_permissions {read write} [current_project]
 OPTRACE "Creating in-memory project" END { }
 OPTRACE "Adding files" START { }
 read_verilog -library xil_defaultlib {
-  C:/Users/ksp5368/Documents/CMPEN331/Lab2/Lab2.srcs/sources_1/new/Design.v
   C:/Users/ksp5368/Documents/CMPEN331/Lab2/Lab2.srcs/sources_1/new/Modules.v
-  C:/Users/ksp5368/Documents/CMPEN331/Lab2/Lab2.srcs/sources_1/new/testbench.v
+  C:/Users/ksp5368/Documents/CMPEN331/Lab2/Lab2.srcs/sources_1/new/Design.v
 }
 OPTRACE "Adding files" END { }
 # Mark all dcp files as not used in implementation to prevent them from being
@@ -104,7 +103,7 @@ read_checkpoint -auto_incremental -incremental C:/Users/ksp5368/Documents/CMPEN3
 close [open __synthesis_is_running__ w]
 
 OPTRACE "synth_design" START { }
-synth_design -top testbench -part xc7z010clg400-1
+synth_design -top DataPath -part xc7z010clg400-1
 OPTRACE "synth_design" END { }
 if { [get_msg_config -count -severity {CRITICAL WARNING}] > 0 } {
  send_msg_id runtcl-6 info "Synthesis results are not added to the cache due to CRITICAL_WARNING"
@@ -114,10 +113,10 @@ if { [get_msg_config -count -severity {CRITICAL WARNING}] > 0 } {
 OPTRACE "write_checkpoint" START { CHECKPOINT }
 # disable binary constraint mode for synth run checkpoints
 set_param constraints.enableBinaryConstraints false
-write_checkpoint -force -noxdef testbench.dcp
+write_checkpoint -force -noxdef DataPath.dcp
 OPTRACE "write_checkpoint" END { }
 OPTRACE "synth reports" START { REPORT }
-create_report "synth_1_synth_report_utilization_0" "report_utilization -file testbench_utilization_synth.rpt -pb testbench_utilization_synth.pb"
+create_report "synth_1_synth_report_utilization_0" "report_utilization -file DataPath_utilization_synth.rpt -pb DataPath_utilization_synth.pb"
 OPTRACE "synth reports" END { }
 file delete __synthesis_is_running__
 close [open __synthesis_is_complete__ w]
--- Lab2/Lab2.runs/synth_1/runme.sh
+++ Lab2/Lab2.runs/synth_1/runme.sh
@@ -40,4 +40,4 @@ EAStep()
      fi
 }
 
-EAStep vivado -log testbench.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source testbench.tcl
+EAStep vivado -log DataPath.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source DataPath.tcl
